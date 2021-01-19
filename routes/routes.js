8d703f584938e368cde9fca96a02bde4f23f2d8e
Joseph Maninang <letsbeedevJoseph@gmail.com>
2021-01-19 10:32:03 +0800

    working api CRUD exept upload

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,6 @@ const cors = require("cors");
 const multer  = require('multer');
  
 app.use(cors());
-
   router.post('/api/imageUpload',function(req,res){
     singleUpload(req,res, function(err){
     return  res.json({'imageUrl': req.file.location})
@@ -26,6 +25,7 @@ app.use(cors());
   })
   
 router.get('/',ItemsController.getAll)
+router.get('/api/item/:ItemId', ItemsController.getItem);
 router.get('/api/getBatchItems/:batchNo',ItemsController.getitems);
 router.post('/api/updateStatus/:batchNo',ItemsController.updateStatus);
 router.post('/api/updateItem/:id',verifyToken,ItemsController.updateItem);
@@ -35,6 +35,16 @@ router.get('/api/getBatchLastNo/:batchNo',ItemsController.getBatchLastNo);
 
 
 
+router.get('/api/batchNum/batch', ItemsController.getBatchesList)
+
+router.post('/api/item/add', ItemsController.postAddItem);
+router.get('/api/edit/:ItemId', ItemsController.getEditItem);
+router.patch('/api/edit/:ItemId', ItemsController.postEditItem);
+router.delete('/api/delete/:ItemId', ItemsController.deleteItemId);
+
+router.post('/api/upload', ItemsController.uploadFile);
+
+// end Item Controllers
 
 router.post('/api/GetSMSbyDate',Controller.getAllbyDate)
 router.get('/api/inboundSMS',Controller.inboundSMS)
@@ -49,7 +59,6 @@ router.delete('/api/deleteUser/:id',UserController.Delete)
 router.post('/api/updateProfile/:id',verifyToken,UserController.updateProfile);
 router.post('/api/updatePassword/:id',verifyToken,UserController.updatePassword);
 router.post('/api/updateUser/:id',verifyToken,UserController.updateUser);
- 
 
 router.post('/api/login',AuthController.login);
 function verifyToken(req,res,next)
