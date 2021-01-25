83de31d588cff25be6c27ad8e663565475f97fe3
ian-magat <letsbeedevian@gmail.com>
2021-01-25 13:09:47 +0800

    prod con string

--- config/db.config.js
+++ config/db.config.js
@@ -2,16 +2,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    // HOST: "letsbeeriders.c4rxjr6wovbq.us-east-2.rds.amazonaws.com",
-    // USER: "admin",
-    // PASSWORD: "adminpassword",
-    // DB: "letsbeeRiderDB",
+    HOST: "database-tracker.c4rxjr6wovbq.us-east-2.rds.amazonaws.com",
+    USER: "admin",
+    PASSWORD: "adminpassword",
+    DB: "DB_letsbee-Tracker",
 
     // // LOCAL
-    HOST: 'localhost',
-    USER:'root',
-    PASSWORD: '',
-    DB: 'DB_letsbee-Tracker',
+    // HOST: 'localhost',
+    // USER:'root',
+    // PASSWORD: '',
+    // DB: 'DB_letsbee-Tracker',
 
     dialect: "mysql",
     pool: {
